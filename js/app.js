15b5d867babd89e733a39164c2db9b4c7bd31900
Charles Clemens <cclemensjr@gmail.com>
2019-03-26 14:24:38 -0700

    Added array of questions

--- js/app.js
+++ js/app.js
@@ -7,4 +7,15 @@ function getName()
     return userName;
 }
 
+function askQuestions(question)
+{
+    let questions = ['What branch of the military did I serve in?',
+                     'What martial art have I studied?',
+                     'What is my favorite movie?',
+                     'What is my favorite book?',
+                     'What is my favorite video game?'];
+    
+    
+}
+
 getName();

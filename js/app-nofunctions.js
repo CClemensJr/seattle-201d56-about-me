c3fb7b005793e79e2077bb235adb06572612817d
Charles Clemens <cclemensjr@gmail.com>
2019-03-27 13:44:38 -0700

    Declared variabls

--- js/app-nofunctions.js
+++ js/app-nofunctions.js
@@ -1,1 +1,11 @@
-'use strict';
+"use strict";
+
+let totalScore = 0;
+let trueFalseQuestions = ["Did I serve in the military?",
+                          "Do I study martial arts?",
+                          "Do I play an instrument?",
+                          "Have I written a novel?",
+                          "Am I afraid of failure?"];
+let trueFalseAnswers = ["YES", "YES", "YES", "YES", "NO"];
+
+let userName = prompt("What is your name");

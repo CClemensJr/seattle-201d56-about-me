f8dc83fd42f016be552222a2e271ce42f76029a2
Charles Clemens <cclemensjr@gmail.com>
2019-03-27 13:51:57 -0700

    added function to get name

--- js/app-nofunctions.js
+++ js/app-nofunctions.js
@@ -9,3 +9,15 @@ let trueFalseQuestions = ["Did I serve in the military?",
 let trueFalseAnswers = ["YES", "YES", "YES", "YES", "NO"];
 
 let userName = prompt("What is your name");
+
+while(userName === null)
+{
+    userName = prompt("What is your name");
+}
+
+console.log(`USERNAME: ${ userName }`);
+
+for (let i = 0; i < trueFalseQuestions.Length; i++)
+{
+    console.log(`QUESTION ${ i + 1 }: ${ trueFalseQuestions[i]}`);
+}

7ca5606a4b108d87006fb8c3f8ea9579ddc5f2a4
xialiu1988 <liuxia198896@gmail.com>
2019-03-28 13:53:51 -0700

    updatd

--- js/app.js
+++ js/app.js
@@ -2,7 +2,11 @@
 const button = document.getElementById('takeQuiz');
 
 button.addEventListener('click', event => {
-  let totalScore = 0;
+  // Score counter
+  let score = 0;
+
+  // Question counter
+  let totalQuestions = 7;
   // This function prompts the user to enter their name and returns the name to the function that calls it.
   function getName()
   {
@@ -14,126 +18,139 @@ button.addEventListener('click', event => {
   // This function takes a number and uses that as an index to reference a question.
   // It then prompts the user for an answer to the question
   // Once an answer is obtained, it sends the question and the answer to the verifyAnswer method
-  function askQuestions(question)
+  function askTrueOrFalseQuestions()
   {
-    let myQuestions = ['What branch of the military did I serve in?',
-      'What martial art have I studied?',
-      'What is my favorite movie?',
-      'What is my favorite book?',
-      'What is my favorite video game?'];
-
-    let userAnswer = prompt(`${ myQuestions[question] }`);
-
-    console.log(`Question: ${ myQuestions[question] }`);
-    console.log(`User Answer: ${ userAnswer }`)
-
-    verifyAnswer(myQuestions[question], userAnswer);
-  }
-
-  // This method takes a question and an answer notifies the user if their answer was correct.
-  function verifyAnswer(myQuestion, userAnswer)
-  {
-    switch (myQuestion)
+    // True or False Questions
+    let trueFalseQuestions = ['Did I serve in the military?',
+      'Do I study martial arts?',
+      'Do I play an instrument?',
+      'Did I write a novel?',
+      'Do I fear failure?'];
+    // True or False Answers
+    let trueFalseAnswers = ['YES', 'YES', 'YES', 'YES', 'NO'];
+    /************
+     * Ask the user yes or no questions
+     */
+    for (let i = 0; i < trueFalseQuestions.length; i++)
     {
-    case 'What branch of the military did I serve in?' :
-      if (userAnswer.toUpperCase() === 'USAF' ||
-                userAnswer.toUpperCase() === 'U.S. AIR FORCE' ||
-                userAnswer.toUpperCase() === 'AIR FORCE' ||
-                userAnswer.toUpperCase() === 'CHAIR FORCE')
-      {
-        alert(`That is correct! I served 6 years in the ${ userAnswer }`);
-        console.log('The user answered correctly.');
+      let userAnswer = prompt(`${ trueFalseQuestions[i] }`);
 
-        getScore(1);
+      console.log(`QUESTION ${ i + 1 }: ${ trueFalseQuestions[i] }`);
+      console.log(`USER'S ANSWER: ${ userAnswer }`);
+
+      if (userAnswer.toUpperCase() === trueFalseAnswers[i])
+      {
+        alert('That is correct!');
+
+        console.log('The user answered correctly');
+
+        score += 1;
       }
       else
       {
-        alert('I\'m sorry, but that is incorrect. I served in the Air Force');
-      }
-
-      break;
-
-    case 'What martial art have I studied?' :
-      if (userAnswer.toUpperCase() === 'TAE KWAN DO' ||
-                userAnswer.toUpperCase() === 'AIKIDO' ||
-                userAnswer.toUpperCase() === 'KARATE' ||
-                userAnswer.toUpperCase() === 'WING CHUN' ||
-                userAnswer.toUpperCase() === 'JEET KUNE DO')
-      {
-        alert('That is correct! I have studied Tae Kwan Do, Aikido, Karate, Wing Chun, and Jeet Kune Do.');
-        console.log('The user answered correctly.');
-
-        getScore(1);
-      }
-      else
-      {
-        alert('I\'m sorry, but that is incorrect. I have studied Tae Kwan Do, Aikido, Karate, Wing Chun, and Jeet Kune Do.');
+        alert('I\'m sorry, that was incorrect.');
         console.log('The user answered incorrectly');
       }
 
-      break;
 
-    case 'What is my favorite movie?' :
-      if (userAnswer.toUpperCase() === 'THE THING')
-      {
-        alert(`That is correct! My favorite movie is ${ userAnswer }`);
-        console.log('The user answered correctly.');
-
-        getScore(1);
-      }
-      else
-      {
-        alert('I\'m sorry, but that is incorrect. My favorite movie is John Carpenter\'s The Thing.');
-        console.log('The user answered incorrectly');
-      }
-
-      break;
-
-    case 'What is my favorite book?' :
-      if (userAnswer.toUpperCase() === 'THE LONG WALK')
-      {
-        alert(`That is correct! My favorite book is ${ userAnswer }`);
-        console.log('The user answered correctly.');
-
-        getScore(1);
-      }
-      else
-      {
-        alert('I\'m sorry, but that is incorrect. My favorite book is The Long Walk by Richard Bachman.');
-        console.log('The user answered incorrectly');
-      }
-
-      break;
-
-    case 'What is my favorite video game?' :
-      if (userAnswer.toUpperCase() === 'NO MAN\'S SKY')
-      {
-        alert(`That is correct! My favorite videogame is ${ userAnswer }`);
-        console.log('The user answered correctly.');
-
-        getScore(1);
-      }
-      else
-      {
-        alert('I\'m sorry, but that is incorrect. My favorite videogame is currently No Man\'s Sky');
-        console.log('The user answered incorrectly');
-      }
-
-      break;
-
-    default :
-      console.log('A question was not successfully loaded');
-
-      break;
+      console.log(`USER'S SCORE: ${ score }`);
     }
   }
 
+  function askNumericalQuesion(){
+
+    // Numerical Guess Answer
+    let numberGuessAnswer = 3;
+    // Numerical Guess Question
+    let numberGuessQuestion = 'How many novels have I written?';
+    /*****************
+     * Ask the user a numerical guess question
+     */
+    let userAnswer = prompt(`${ numberGuessQuestion }`);
+
+    console.log(`QUESTION 6: ${ numberGuessQuestion }`);
+    console.log(`USER'S ANSWER: ${ userAnswer }`);
+
+    if (parseInt(userAnswer) === numberGuessAnswer)
+    {
+      alert('That was correct!');
+      console.log('The user answered correctly');
+
+      score += 1;
+
+      console.log(`USER'S SCORE: ${ score }`);
+    }
+    else
+    {
+      for (let i = 0; i < 3; i++)
+      {
+        if (parseInt(userAnswer) === numberGuessAnswer)
+        {
+          alert('That was correct!');
+          console.log('The user answered correctly');
+
+          score += 1;
+
+          console.log(`USER'S SCORE: ${ score }`);
+
+          break;
+        }
+        else if (parseInt(userAnswer) > numberGuessAnswer)
+        {
+          alert('Your guess was too high. Guess again.');
+          userAnswer = prompt(`${ numberGuessQuestion }`);
+        }
+        else if (parseInt(userAnswer) < numberGuessAnswer)
+        {
+          alert('Your guess was too low. Guess again.');
+          userAnswer = prompt(`${ numberGuessQuestion }`);
+        }
+      }
+    }
+
+  }
+
+  function askQuestionWithMultiAnswers(){
+    // Multi-Answer Question
+    let multiAnswerQuestion = 'What martial art have I studied?'
+    // MultiAnswer Answers
+    let multiAnswerOptions = ['TAE KWAN DO', 'KARATE', 'AIKIDO', 'WING CHUN', 'JEET KUNE DO'];
+
+    /*****************
+     * Ask the user a question with many possible answers.
+     */
+    for (let i = 0; i < 6; i++)
+    {
+      let userAnswer = prompt(`${ multiAnswerQuestion }`);
+
+      console.log(`QUESTION 7: ${ multiAnswerQuestion }`);
+      console.log(`USER'S ANSWER: ${ userAnswer }`);
+      if (multiAnswerOptions.includes(userAnswer.toUpperCase()))
+      {
+        alert('That was correct!');
+        console.log('The user answered correctly');
+
+        score += 1;
+
+        console.log(`USER'S SCORE: ${ score }`);
+
+        break;
+      }
+      else
+      {
+        alert('I\'m sorry, that was incorrect. Please try again.');
+        console.log('The user answered incorrectly');
+      }
+    }
+
+
+  }
   // This function takes a number and adds it to the score. It then returns the score to the function that calls it.
   function getScore(point)
   {
-    totalScore += point;
+    score += point;
 
-    console.log(`In getScore(). totalScore = ${ totalScore }`);
+    console.log(`In getScore(). score = ${ score }`);
   }
 
   // This method is the main method that calls the other methods.
@@ -141,14 +158,12 @@ button.addEventListener('click', event => {
   {
     let name = getName();
 
-    for (let i = 0; i < 5; i++)
-    {
-      askQuestions(i);
-    }
+    askTrueOrFalseQuestions();
+    askNumericalQuesion();
+    askQuestionWithMultiAnswers();
+    console.log(`The user got ${ score } out of ${totalQuestions} correct.`);
 
-    console.log(`The user got ${ totalScore } out of 5 correct.`);
-
-    alert(`Thank you for completing this questionnaire, ${ name }. You got ${ totalScore } out of 5 correct.` );
+    alert(`Thank you for completing this questionnaire, ${ name }. You got ${ score } out of ${totalQuestions} correct.` );
   }
 
   main();
